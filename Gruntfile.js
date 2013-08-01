dd8889fd2d225bf9cec4574f1985192c63526236
Jaison Justus <jaison.justus.lp@gmail.com>
2013-08-01 15:29:46 +0530

    Change GruntFile.'

--- Gruntfile.js
+++ Gruntfile.js
@@ -304,7 +304,7 @@ module.exports = function (grunt) {
 
         grunt.task.run([
             'clean:server',
-            'concurrent:server',
+            // 'concurrent:server',
             'connect:livereload',
             'open',
             'watch'
